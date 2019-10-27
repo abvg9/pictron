e12dc11d2e3e654c3a49978eb71ae5f10853fad6
alvaro <alvaro@pcalvaro.alvaro.mre>
2019-10-27 14:41:34 +0100

    Added validation logic.

---
+++ lib/logic/validation/string_validator.dart
@@ -0,0 +1,14 @@
+const String format =
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[['
+    r'0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+'
+    r'[a-zA-Z]{2,}))$';
+
+class StringValidator {
+
+  static RegExp _emailRegExp = new RegExp(format);
+
+  bool isEmail(String email) {
+    return _emailRegExp.hasMatch(email);
+  }
+
+}
