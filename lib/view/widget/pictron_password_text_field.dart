7b38aebd7f4444211c7355de81ee8060451d129d
Roberto Pintos López <roberto@secture.com>
2019-10-27 19:23:53 +0100

    Added Pictron text fields.
    Added text field and password text field in order to build an uniform UI style.
    
    Co-authored-by: @abvg9

---
+++ lib/view/widget/pictron_password_text_field.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:pictron/view/widget/pictron_base_text_field.dart';
+
+class PictronPasswordTextField extends PictronBaseTextField {
+  PictronPasswordTextField({
+    Key key,
+    String placeholder,
+  }) : super(
+    key: key,
+    placeholder: placeholder,
+    obscureText: true,
+  );
+}
