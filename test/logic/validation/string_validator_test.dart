2ce10551c1cad485b188a820b2436849f2aeaef8
alvaro <alvaro@pcalvaro.alvaro.mre>
2019-10-28 23:11:17 +0100

    Added test for string_validator.
    Co-authored-by: @notaphplover

---
+++ test/logic/validation/string_validator_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pictron/logic/validation/string_validator.dart';
+
+void stringValidatorTest() {
+  group('String validator tests', () {
+    isEmailCorrect();
+    isEmailIncorrect();
+  });
+}
+
+void isEmailCorrect() {
+
+  test('String validator must detects that the emails are in a correct format', () {
+
+    final StringValidator stringValidator = new StringValidator();
+
+    final fakeEmail = "fakeEmail@hotmail.com";
+
+    expect(stringValidator.isEmail(fakeEmail), true);
+
+  });
+
+}
+
+void isEmailIncorrect() {
+
+  test('String validator must detects that the emails are not in a correct format', () {
+
+    final StringValidator stringValidator = new StringValidator();
+
+    final fakeEmail1 = "fakeEmail-gmail.com";
+    final fakeEmail2 = "fakeEmail@hotmailcom";
+    final fakeEmail3 = "fakeEmailgmail.com";
+
+    expect(stringValidator.isEmail(fakeEmail1), false);
+    expect(stringValidator.isEmail(fakeEmail2), false);
+    expect(stringValidator.isEmail(fakeEmail3), false);
+
+  });
+}
