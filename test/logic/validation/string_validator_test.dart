30a3e5a4fec47086917c46bd69aa6aec6f260ea9
Roberto Pintos López <roberto@secture.com>
2019-10-28 23:18:49 +0100

    Fixed tests.

--- test/logic/validation/string_validator_test.dart
+++ test/logic/validation/string_validator_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pictron/logic/validation/string_validator.dart';
 
-void stringValidatorTest() {
+void main() {
   group('String validator tests', () {
     isEmailCorrect();
     isEmailIncorrect();
