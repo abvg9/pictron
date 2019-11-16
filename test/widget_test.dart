79da2813e9b79cec7db445bd4bf728498e7ba758
FRYoussef <f.r.youssef@hotmail.com>
2019-11-16 21:57:36 +0100

    Fixed test issue

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,6 +13,6 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(App());
 
-    expect(find.text('Hello World'), findsOneWidget);
+    expect(find.text('Log in'), findsOneWidget);
   });
 }
