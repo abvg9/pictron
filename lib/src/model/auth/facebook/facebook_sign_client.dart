3cd3dbfe82cc02ed61504780e40b19829851bd5e
abvg9 <deveavg@gmail.com>
2019-11-25 17:21:43 +0100

    Resolved some merge conlifcts.

--- lib/src/model/auth/facebook/facebook_sign_client.dart
+++ lib/src/model/auth/facebook/facebook_sign_client.dart
@@ -1,18 +1,24 @@
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:pictron/src/model/auth/sign_client.dart';
 
-class FacebookSignClient {
+class FacebookSignClient extends SignClient {
   FacebookSignClient() {
     _facebookLogin = FacebookLogin();
   }
 
   FacebookLogin _facebookLogin;
 
+  @override
   Future<void> handleSignOut() async {
     await _facebookLogin.logOut();
   }
 
+  @override
   Future<void> handleSignIn() async {
     _facebookLogin.loginBehavior = FacebookLoginBehavior.webViewOnly;
     await _facebookLogin.logIn(<String>['email']);
   }
+
+  @override
+  Object getToken() => _facebookLogin.currentAccessToken;
 }
