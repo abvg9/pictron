643857c39b54fe178c2ec15500883b9452d7ded9
abvg9 <deveavg@gmail.com>
2019-12-15 17:17:46 +0100

    Repaired some issues to merge with develop.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mvc_pattern/mvc_pattern.dart';
-
 import 'package:pictron/src/controllers/main_controller.dart';
-import 'package:pictron/src/screens/calendar.dart';
-import 'package:pictron/src/screens/test_screen.dart';
 
 class App extends AppMVC {
   App({Key key}) : super(con: _controller, key: key);
@@ -14,11 +12,17 @@ class App extends AppMVC {
   static String get title => _app.title.toString();
 
   @override
-  Widget build(BuildContext context) => _app = MaterialApp(
-        title: 'PictoTEAsk',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Calendar(),
-      );
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+      DeviceOrientation.landscapeLeft,
+    ]);
+    SystemChrome.setEnabledSystemUIOverlays(<SystemUiOverlay>[]);
+    return _app = MaterialApp(
+      title: 'PictoTEAsk',
+      theme: ThemeData(
+        canvasColor: Colors.cyan[100],
+      ),
+      //home: const LoginPage(),
+    );
+  }
 }
--- lib/src/controllers/main_controller.dart
+++ lib/src/controllers/main_controller.dart
@@ -1,11 +1,7 @@
 import 'package:mvc_pattern/mvc_pattern.dart' show ControllerMVC;
-import 'package:pictron/src/model/tranfers/Task_transfer.dart';
-import 'package:pictron/src/model/Task_logic.dart';
-//import 'package:pictron/src/app.dart' show App;
 
 class Con extends ControllerMVC {
 
-  // Singelton
   factory Con() => _this ??= Con._();
 
   Con._();
@@ -15,8 +11,4 @@ class Con extends ControllerMVC {
   // For easy access in the application
   static Con get con => _this;
 
-  static final TaskLogic _taskLogic = TaskLogic();
-
-  TaskTransfer getTask() => _taskLogic.loadTask(0);
 }
-
